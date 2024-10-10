f3cc2262aa39eb82f3495bf0ebc8a45f8a41b4ef
marcpish <164066578+marcpish@users.noreply.github.com>
2024-10-10 18:05:12 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import requests
+
+api_key='023fa42bbcf8cac2d9ba8272f793dd2f'
+user_input = input("Enter city: ")
+
+weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
+print(weather_data.json())
+
+if weather_data.json()['cod'] == '404':
+    print("No city found")
+else:
+    weather = weather_data.json()['weather'][0]['main']
+    temp = round(weather_data.json()['main']['temp'])
+    country = weather_data.json()['sys']['country']
+    print(f"The temperature in {user_input} is {temp} degrees F")
+    print(f"The country {user_input} is in: {country}")
+
+      
+
